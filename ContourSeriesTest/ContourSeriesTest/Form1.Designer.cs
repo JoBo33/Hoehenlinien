fcec58654e8686f042e31f9e25e816820b143cb1
Bosse <jb83292@win.edag.de>
2021-07-07 08:12:05 +0200

    Create ContourSeries test file with Example

---
+++ ContourSeriesTest/ContourSeriesTest/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace ContourSeriesTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.plotViewTest = new OxyPlot.WindowsForms.PlotView();
+            this.SuspendLayout();
+            // 
+            // plotViewTest
+            // 
+            this.plotViewTest.Location = new System.Drawing.Point(21, 12);
+            this.plotViewTest.Name = "plotViewTest";
+            this.plotViewTest.PanCursor = System.Windows.Forms.Cursors.Hand;
+            this.plotViewTest.Size = new System.Drawing.Size(820, 417);
+            this.plotViewTest.TabIndex = 0;
+            this.plotViewTest.Text = "plotViewtest";
+            this.plotViewTest.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
+            this.plotViewTest.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE;
+            this.plotViewTest.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS;
+            this.plotViewTest.Click += new System.EventHandler(this.plotViewTest_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(887, 450);
+            this.Controls.Add(this.plotViewTest);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private OxyPlot.WindowsForms.PlotView plotViewTest;
+    }
+}
+
---
+++ ContourSeriesTest/ContourSeriesTest/Form1.cs
@@ -0,0 +1,55 @@
+﻿using OxyPlot;
+using OxyPlot.Series;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ContourSeriesTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        PlotModel model = new PlotModel { Title = "ContourSeries" };
+        
+        public void DrawContours()
+        {
+
+            double x0 = 0;
+            double x1 = 6;
+            double y0 = 0;
+            double y1 = 6;
+
+            //generate values
+            Func<double, double, double> peaks = (x, y) => -(Math.Pow(x - 4, 2) + Math.Pow(y - 4, 2)) + 4;
+            double[] xx = ArrayBuilder.CreateVector(x0, x1, 100);
+            double[] yy = ArrayBuilder.CreateVector(y0, y1, 100);
+            double[,] peaksData = ArrayBuilder.Evaluate(peaks, xx, yy);
+
+            ContourSeries cs = new ContourSeries
+            {
+                Color = OxyColors.Black,
+                LabelBackground = OxyColors.White,
+                ColumnCoordinates = yy,
+                RowCoordinates = xx,
+                Data = peaksData
+            };
+            model.Series.Add(cs);
+        }
+
+        private void plotViewTest_Click(object sender, EventArgs e)
+        {
+            plotViewTest.Model = model;
+            DrawContours();
+        }
+    }
+}
