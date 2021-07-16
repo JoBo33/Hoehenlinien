bc6467d49acc14cf1b642c81cd4373cbf92f498e
Bosse <joelbosse@gmx.de>
2021-07-16 12:55:25 +0200

    Update project

--- ContourMap/ContourMap/Form1.Designer.cs
+++ ContourMap/ContourMap/Form1.Designer.cs
@@ -29,23 +29,23 @@
         private void InitializeComponent()
         {
             this.plotViewContours = new OxyPlot.WindowsForms.PlotView();
-            this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.plotViewColumnSeries1 = new OxyPlot.WindowsForms.PlotView();
             this.buttonVolume = new System.Windows.Forms.Button();
             this.buttonTrucks = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.NumberOfTrucks = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.NeededTime = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBoxVolume = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.tabControl1.SuspendLayout();
-            this.tabPage1.SuspendLayout();
+            this.textBoxFilePath = new System.Windows.Forms.TextBox();
+            this.buttonDiagram = new System.Windows.Forms.Button();
+            this.tabPageP1 = new System.Windows.Forms.TabPage();
+            this.plotViewColumnSeries1 = new OxyPlot.WindowsForms.PlotView();
+            this.tabControlHillProfiles = new System.Windows.Forms.TabControl();
+            this.NumberOfTrucks = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NeededTime = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabPageP1.SuspendLayout();
+            this.tabControlHillProfiles.SuspendLayout();
             this.SuspendLayout();
             // 
             // plotViewContours
@@ -62,54 +62,9 @@
             this.plotViewContours.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE;
             this.plotViewContours.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS;
             // 
-            // tabControl1
-            // 
-            this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(32, 37);
-            this.tabControl1.Name = "tabControl1";
-            this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(505, 340);
-            this.tabControl1.TabIndex = 2;
-            // 
-            // tabPage1
-            // 
-            this.tabPage1.Controls.Add(this.plotViewColumnSeries1);
-            this.tabPage1.Location = new System.Drawing.Point(4, 25);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(497, 311);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
-            this.tabPage1.UseVisualStyleBackColor = true;
-            // 
-            // tabPage2
-            // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 25);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(497, 311);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
-            this.tabPage2.UseVisualStyleBackColor = true;
-            // 
-            // plotViewColumnSeries1
-            // 
-            this.plotViewColumnSeries1.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.plotViewColumnSeries1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.plotViewColumnSeries1.Location = new System.Drawing.Point(3, 3);
-            this.plotViewColumnSeries1.Name = "plotViewColumnSeries1";
-            this.plotViewColumnSeries1.PanCursor = System.Windows.Forms.Cursors.Hand;
-            this.plotViewColumnSeries1.Size = new System.Drawing.Size(491, 305);
-            this.plotViewColumnSeries1.TabIndex = 2;
-            this.plotViewColumnSeries1.Text = "plotView1";
-            this.plotViewColumnSeries1.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
-            this.plotViewColumnSeries1.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE;
-            this.plotViewColumnSeries1.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS;
-            // 
             // buttonVolume
             // 
-            this.buttonVolume.Location = new System.Drawing.Point(645, 203);
+            this.buttonVolume.Location = new System.Drawing.Point(645, 211);
             this.buttonVolume.Name = "buttonVolume";
             this.buttonVolume.Size = new System.Drawing.Size(262, 53);
             this.buttonVolume.TabIndex = 3;
@@ -135,42 +90,32 @@
             this.NeededTime});
             this.dataGridView1.Location = new System.Drawing.Point(645, 427);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
             this.dataGridView1.RowTemplate.Height = 24;
             this.dataGridView1.Size = new System.Drawing.Size(262, 329);
             this.dataGridView1.TabIndex = 5;
             // 
-            // NumberOfTrucks
-            // 
-            this.NumberOfTrucks.HeaderText = "Number of trucks";
-            this.NumberOfTrucks.Name = "NumberOfTrucks";
-            this.NumberOfTrucks.ReadOnly = true;
-            // 
-            // NeededTime
-            // 
-            this.NeededTime.HeaderText = "Needed time";
-            this.NeededTime.Name = "NeededTime";
-            this.NeededTime.ReadOnly = true;
-            // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(647, 267);
+            this.label1.Location = new System.Drawing.Point(647, 275);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(21, 17);
             this.label1.TabIndex = 6;
             this.label1.Text = "V:";
             // 
-            // textBox1
+            // textBoxVolume
             // 
-            this.textBox1.Location = new System.Drawing.Point(674, 262);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
-            this.textBox1.TabIndex = 7;
+            this.textBoxVolume.Location = new System.Drawing.Point(674, 270);
+            this.textBoxVolume.Name = "textBoxVolume";
+            this.textBoxVolume.ReadOnly = true;
+            this.textBoxVolume.Size = new System.Drawing.Size(100, 22);
+            this.textBoxVolume.TabIndex = 7;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(780, 267);
+            this.label2.Location = new System.Drawing.Point(780, 275);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(24, 17);
             this.label2.TabIndex = 8;
@@ -185,33 +130,90 @@
             this.label3.TabIndex = 9;
             this.label3.Text = "Insert the path of the file here";
             // 
-            // textBox2
+            // textBoxFilePath
             // 
-            this.textBox2.Location = new System.Drawing.Point(645, 94);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(262, 22);
-            this.textBox2.TabIndex = 10;
+            this.textBoxFilePath.Location = new System.Drawing.Point(645, 94);
+            this.textBoxFilePath.Name = "textBoxFilePath";
+            this.textBoxFilePath.Size = new System.Drawing.Size(262, 22);
+            this.textBoxFilePath.TabIndex = 10;
+            // 
+            // buttonDiagram
+            // 
+            this.buttonDiagram.Location = new System.Drawing.Point(645, 122);
+            this.buttonDiagram.Name = "buttonDiagram";
+            this.buttonDiagram.Size = new System.Drawing.Size(262, 61);
+            this.buttonDiagram.TabIndex = 11;
+            this.buttonDiagram.Text = "Draw diagrams";
+            this.buttonDiagram.UseVisualStyleBackColor = true;
+            this.buttonDiagram.Click += new System.EventHandler(this.buttonDiagram_Click);
+            // 
+            // tabPageP1
+            // 
+            this.tabPageP1.Controls.Add(this.plotViewColumnSeries1);
+            this.tabPageP1.Location = new System.Drawing.Point(4, 25);
+            this.tabPageP1.Name = "tabPageP1";
+            this.tabPageP1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageP1.Size = new System.Drawing.Size(497, 311);
+            this.tabPageP1.TabIndex = 0;
+            this.tabPageP1.Text = "Profile 1";
+            this.tabPageP1.UseVisualStyleBackColor = true;
+            // 
+            // plotViewColumnSeries1
+            // 
+            this.plotViewColumnSeries1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.plotViewColumnSeries1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.plotViewColumnSeries1.Location = new System.Drawing.Point(3, 3);
+            this.plotViewColumnSeries1.Name = "plotViewColumnSeries1";
+            this.plotViewColumnSeries1.PanCursor = System.Windows.Forms.Cursors.Hand;
+            this.plotViewColumnSeries1.Size = new System.Drawing.Size(491, 305);
+            this.plotViewColumnSeries1.TabIndex = 2;
+            this.plotViewColumnSeries1.Text = "plotView1";
+            this.plotViewColumnSeries1.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
+            this.plotViewColumnSeries1.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE;
+            this.plotViewColumnSeries1.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS;
+            // 
+            // tabControlHillProfiles
+            // 
+            this.tabControlHillProfiles.Controls.Add(this.tabPageP1);
+            this.tabControlHillProfiles.Location = new System.Drawing.Point(32, 37);
+            this.tabControlHillProfiles.Name = "tabControlHillProfiles";
+            this.tabControlHillProfiles.SelectedIndex = 0;
+            this.tabControlHillProfiles.Size = new System.Drawing.Size(505, 340);
+            this.tabControlHillProfiles.TabIndex = 2;
+            // 
+            // NumberOfTrucks
+            // 
+            this.NumberOfTrucks.HeaderText = "Number of trucks";
+            this.NumberOfTrucks.Name = "NumberOfTrucks";
+            this.NumberOfTrucks.ReadOnly = true;
+            // 
+            // NeededTime
+            // 
+            this.NeededTime.HeaderText = "Needed time";
+            this.NeededTime.Name = "NeededTime";
+            this.NeededTime.ReadOnly = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1018, 786);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.buttonDiagram);
+            this.Controls.Add(this.textBoxFilePath);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBoxVolume);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.buttonTrucks);
             this.Controls.Add(this.buttonVolume);
-            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.tabControlHillProfiles);
             this.Controls.Add(this.plotViewContours);
             this.Name = "Form1";
-            this.Text = "Form1";
-            this.tabControl1.ResumeLayout(false);
-            this.tabPage1.ResumeLayout(false);
+            this.Text = "Hillvolume and contour lines";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabPageP1.ResumeLayout(false);
+            this.tabControlHillProfiles.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -220,20 +222,20 @@
         #endregion
 
         private OxyPlot.WindowsForms.PlotView plotViewContours;
-        private System.Windows.Forms.TabControl tabControl1;
-        private System.Windows.Forms.TabPage tabPage1;
-        private OxyPlot.WindowsForms.PlotView plotViewColumnSeries1;
-        private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.Button buttonVolume;
         private System.Windows.Forms.Button buttonTrucks;
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn NumberOfTrucks;
-        private System.Windows.Forms.DataGridViewTextBoxColumn NeededTime;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBoxVolume;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBoxFilePath;
+        private System.Windows.Forms.Button buttonDiagram;
+        private System.Windows.Forms.TabPage tabPageP1;
+        private OxyPlot.WindowsForms.PlotView plotViewColumnSeries1;
+        private System.Windows.Forms.TabControl tabControlHillProfiles;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NumberOfTrucks;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NeededTime;
     }
 }
 
--- ContourMap/ContourMap/Form1.cs
+++ ContourMap/ContourMap/Form1.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
+using OxyPlot.WindowsForms;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,16 +20,10 @@ namespace ContourMap
         public Form1()
         {
             InitializeComponent();
-            //StreamReader str = new StreamReader(textBox2.Text);
-            //string line = str.ReadLine();
-            //FillData(line);
-            //adjustHeight();
-            
         }
 
-        List<double[]> data = new List<double[]> { };
         
-        public void FillData(string line)
+        public void FillData(string line, List<double[]> data)
         {
             string point = "";
             string number = "";
@@ -70,7 +68,7 @@ namespace ContourMap
             }
         }
 
-        public void adjustHeight()
+        public void adjustHeight(List<double[]> data)
         {
             double smallestHeight = double.MaxValue;
             for (int i = 0; i < data.Count; i++)
@@ -86,22 +84,26 @@ namespace ContourMap
             }
         }
 
-        int pointsInOneRow = 0;
-        public double CalculateVolume()
+
+        public void DeterminePointsInOneRow(List<double[]> data, ref int pointsInOneRow)
         {
-            double volume = 0;
-            for (int i = 0; i <= data.Count; i += (1))
-		    {
+            for (int i = 0; i < data.Count; i += (1))
+            {
                 pointsInOneRow = pointsInOneRow + 1;
                 if (data[i][1] != data[i + 1][1])
                 {
                     break;
                 }
             }
+        }
+
+        public double CalculateVolume(List<double[]> data, int pointsInOneRow)
+        {
+            double volume = 0;
             double ground = Math.Pow((data[1][0] - data[0][0]),2);
-            for (int i = 0; i <= data.Count - pointsInOneRow; i += (1))
+            for (int i = 0; i < data.Count - pointsInOneRow-1; i += (1))
 		    {
-                if (i % pointsInOneRow == 0)
+                if((i % pointsInOneRow)-(pointsInOneRow-1) == 0 && i != 0)
                 {
                     continue;
                 }
@@ -111,20 +113,23 @@ namespace ContourMap
             return volume;
         }
 
-        public void DetermineContourLines()
+        public void DetermineContourLines(List<double[]> data, PlotModel model, int pointsInOneRow)
         {
-            for (int i = 0; i <= data[2].Max(); i += (1))
+            for (int i = 0; i <= data[2].Max(); i += (1)) // funktion für max hoehe implementieren!!
             {
                 List<double[]> contourLine = new List<double[]> { };
-                for (int j = 0; j <= data.Count - pointsInOneRow; j += (1))
+                for (int j = 0; j < data.Count-1; j += (1))
                 {
-                    if (data[j][2] < i && data[j + pointsInOneRow][2] > i || data[j][2] > i && data[j + pointsInOneRow][2] < i)
+                    if (j < data.Count - pointsInOneRow)
                     {
-                        double tmp = (i - data[j][2]) / (data[j + pointsInOneRow][2] - data[j][2]);
-                        double[] pointOnHeight = { data[j][0] + tmp * (data[j + pointsInOneRow][0] - data[j][0]), data[j][1] + tmp * (data[j + pointsInOneRow][1] - data[j][1]), i };
-                        contourLine.Add(pointOnHeight); //add pointOnHeight to contourLine
+                        if (data[j][2] < i && data[j + pointsInOneRow][2] > i || data[j][2] > i && data[j + pointsInOneRow][2] < i)
+                        {
+                            double tmp = (i - data[j][2]) / (data[j + pointsInOneRow][2] - data[j][2]);
+                            double[] pointOnHeight = { data[j][0] + tmp * (data[j + pointsInOneRow][0] - data[j][0]), data[j][1] + tmp * (data[j + pointsInOneRow][1] - data[j][1]), i };
+                            contourLine.Add(pointOnHeight); //add pointOnHeight to contourLine
+                        }
                     }
-                    if (j % pointsInOneRow == 0)
+                    if ((j % pointsInOneRow) - (pointsInOneRow - 1) == 0 && j != 0) //(j % pointsInOneRow == 0)
                     {
                         continue;
                     }
@@ -135,23 +140,224 @@ namespace ContourMap
                         contourLine.Add(pointOnHeight);
                     }
                 }
+                if (contourLine.Count != 0)
+                {
+                    sortContourPoints(ref contourLine);
+                    CatmullRomSplines(contourLine, model);
+
+                }
                 //draw contourLine;
             }
         }
 
-        private void buttonVolume_Click(object sender, EventArgs e)
+        private void CatmullRomSplines(List<double[]> contourLine, PlotModel model)
         {
-            StreamReader str = new StreamReader(textBox2.Text);
-            string line = str.ReadLine();
-            FillData(line);
-            adjustHeight();
-            double volume = CalculateVolume();
-            textBox1.Text = volume.ToString();
+            LineSeries line = new LineSeries();
+            for (float t = 0; t < contourLine.Count - 1; t += 0.01f)
+            {
+                int p1 = (int)t;
+                int p2 = (p1 + 1) % (contourLine.Count - 1);
+                int p3 = (p2 + 1) % (contourLine.Count - 1);
+                int p0 = p1 >= 1 ? p1 - 1 : contourLine.Count - 1;
+
+                float tmp = t - (int)t;
+                float tt = tmp * tmp;
+                float ttt = tmp * tmp * tmp;
+
+                float q1 = -ttt + 2.0f * tt - tmp;
+                float q2 = 3.0f * ttt - 5.0f * tt + 2;
+                float q3 = -3.0f * ttt + 4.0f * tt + tmp;
+                float q4 = ttt - tt;
+
+                double x = 0.5 * (contourLine[p0][0] * q1 + contourLine[p1][0] * q2 + contourLine[p2][0] * q3 + contourLine[p3][0] * q4);
+                double y = 0.5 * (contourLine[p0][1] * q1 + contourLine[p1][1] * q2 + contourLine[p2][1] * q3 + contourLine[p3][1] * q4);
+
+                line.Points.Add(new DataPoint(x, y));
+            }
+            model.Series.Add(line);
+            model.InvalidatePlot(true);
+        }
+
+        public void sortContourPoints(ref List<double[]> contourLine)
+        {
+            List<double[]> sortedContourLine = new List<double[]> { };
+            for (int i = 0; i < contourLine.Count; i++)
+            {
+                for (int j = 0; j < contourLine.Count-1; j++)
+                {
+                    if(contourLine[j][0] > contourLine[j+1][0])
+                    {
+                       Swap(ref contourLine, j, j + 1);
+                    }
+                }
+            }
+            for (int i = 0; i < contourLine.Count-1; i++)
+            {
+                if (contourLine[i][0] == contourLine[i + 1][0] && contourLine[i][1] > contourLine[i + 1][1])
+                {
+                    Swap(ref contourLine, i, i + 1);
+                }
+            }
+            for (int i = 0; i < contourLine.Count; i+=2)
+            {
+                sortedContourLine.Add(contourLine[i]);
+            }
+            for (int i = contourLine.Count-1; i >= 0; i -= 2)
+            {
+                sortedContourLine.Add(contourLine[i]);
+            }
+            contourLine = sortedContourLine;
+        }
+        public static void Swap(ref List<double[]> sortingList, int indexA, int indexB)
+        {
+            double[] temp = sortingList[indexA];
+            sortingList[indexA] = sortingList[indexB];
+            sortingList[indexB] = temp;
+        }
+            private void buttonVolume_Click(object sender, EventArgs e)
+        {
+            PrepareVariables(1);
         }
 
         private void buttonTrucks_Click(object sender, EventArgs e)
         {
-            
+            PrepareVariables(2);
         }
+
+        private void buttonDiagram_Click(object sender, EventArgs e)
+        {
+            PrepareVariables(0);
+        }
+
+
+        private void PrepareVariables(int pressedButton)
+        {
+
+            List<double[]> data = new List<double[]> { };
+            if (textBoxFilePath.Text == string.Empty)
+            {
+                MessageBox.Show("Pls enter a file path.");
+                return;
+            }
+
+            StreamReader str;
+            try
+            {
+                str = new StreamReader(textBoxFilePath.Text);
+                string line = str.ReadLine();
+                FillData(line, data);
+            }
+            catch(FileNotFoundException e)
+            {
+                MessageBox.Show("The file " + e.FileName+ " could not be found, please check the file path.");
+                return;
+            }
+            int pointsInOneRow = 0;
+            SortData(data);
+            adjustHeight(data);
+            DeterminePointsInOneRow(data, ref pointsInOneRow);
+
+
+            if (pressedButton == 0)
+            {
+                PlotModel model = new PlotModel();
+                plotViewContours.Model = model;
+                DetermineContourLines(data, model, pointsInOneRow);
+
+                PlotModel firstTabModel = new PlotModel();
+                plotViewColumnSeries1.Model = firstTabModel;
+                DrawColumnSeries(ref firstTabModel, data, pointsInOneRow);
+
+                for (int i = 1; i < pointsInOneRow; i++)
+                {
+                    string title = "Profile " + (tabControlHillProfiles.TabCount + 1).ToString();
+                    TabPage tabPageP2 = new TabPage(title);
+                    tabControlHillProfiles.TabPages.Add(tabPageP2);
+
+                    PlotModel plot = new PlotModel();
+                    PlotView plotView2 = new PlotView() { BackColor = Color.White, Dock = DockStyle.Fill};
+                    plotView2.Model = plot;
+                    DrawColumnSeries(ref plot, data, pointsInOneRow, i);
+
+                    tabPageP2.Controls.Add(plotView2);
+                }
+            }
+            else if(pressedButton == 1)
+            {
+                double volume = CalculateVolume(data, pointsInOneRow);
+                textBoxVolume.Text = volume.ToString();
+            }
+            else if(pressedButton == 2)
+            {
+                double volume = CalculateVolume(data, pointsInOneRow);
+                int numberOfTrucks = 100;
+                dataGridView1.RowCount = numberOfTrucks;
+                for (int i = 1; i <= numberOfTrucks; i++)
+                {
+                    dataGridView1.Rows[i - 1].Cells[0].Value = i;
+                    dataGridView1.Rows[i - 1].Cells[1].Value = Math.Ceiling((volume / 7) / i) * 30;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Sth. goes wrong.");
+            }
+        }
+
+        private void SortData(List<double[]> data)
+        {
+            for (int i = 0; i < data.Count; i++)
+            {
+                for (int j = 0; j < data.Count - 1; j++)
+                {
+                    if (data[j][1] > data[j + 1][1])
+                    {
+                        Swap(ref data, j, j + 1);
+                    }
+                }
+            }
+            for (int i = 0; i < data.Count - 1; i++)
+            {
+                if (data[i][1] == data[i + 1][1] && data[i][0] > data[i + 1][0])
+                {
+                    Swap(ref data, i, i + 1);
+                }
+            }
+        }
+
+        private void DrawColumnSeries(ref PlotModel plot, List<double[]> data, int pointsInOneRow, int rowNumber = 0)
+        {
+            double maxHeight = FindMaxHeight(data);
+            ColumnSeries columnSeries = new ColumnSeries();
+            LinearAxis height = new LinearAxis { Minimum = 0, Maximum = maxHeight };
+            height.IsZoomEnabled = false;
+            plot.Axes.Add(height);
+
+            CategoryAxis xAxis = new CategoryAxis { };
+            xAxis.IsZoomEnabled = false;
+            plot.Axes.Add(xAxis);
+
+            for (int i = 0; i < pointsInOneRow; i++)
+            {
+                columnSeries.Items.Add(new ColumnItem(data[i + pointsInOneRow * rowNumber][2]));
+            }
+            plot.Series.Add(columnSeries);
+            plot.InvalidatePlot(true);
+
+        }
+
+        private double FindMaxHeight(List<double[]> data)
+        {
+            double max = 0;
+            for (int i = 0; i < data.Count; i++)
+            {
+                if (max < data[i][2])
+                {
+                    max = data[i][2];
+                }
+            }
+            return max;
+        }
+
     }
 }
